96dd89261e7245eaa2e52d876f74c530ba7d7e63
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-07-30 20:13:46 -0400

    refactoring test

--- src/main/java/pagepackage/CheckBox.java
+++ src/main/java/pagepackage/CheckBox.java
@@ -5,36 +5,43 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import basepackage.BaseTest;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class CheckBox extends BaseTest {
-	
-	public CheckBox() {
-		PageFactory.initElements(driver, this);
-	}
-	
-	@FindBy (xpath="//*[@id='checkboxes']/input[1]")
-	private static WebElement UncheckedCheckBox;
-	
-	@FindBy(xpath="//*[@id='checkboxes']/input[2]")
-	private static WebElement CheckedCheckBox;
-	
-	public void UncheckedCheckBox() {
-		UncheckedCheckBox.click();
-	}
-	
-	public void CheckedCheckBox() {
-		CheckedCheckBox.click();
-	}
-	
-	public boolean AssertionCheckBox() {
-		
-		if(UncheckedCheckBox.isSelected()== false) {
-			return true;
-		}
-		else if(CheckedCheckBox.isSelected() == true) {
-		    return true;
-		}
-		return false;
-		}
+
+    public CheckBox() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id='checkboxes']/input[1]")
+    private static WebElement UncheckedCheckBox;
+
+    @FindBy(xpath = "//*[@id='checkboxes']/input[2]")
+    private static WebElement CheckedCheckBox;
+
+    public void UncheckedCheckBox() {
+        wait = new WebDriverWait(driver, 100);
+        CheckedCheckBox = wait.until(ExpectedConditions.elementToBeClickable(CheckedCheckBox));
+        UncheckedCheckBox.click();
+    }
+
+    public void CheckedCheckBox() {
+        wait = new WebDriverWait(driver, 100);
+        CheckedCheckBox = wait.until(ExpectedConditions.elementToBeClickable(CheckedCheckBox));
+        CheckedCheckBox.click();
+    }
+
+    public boolean AssertionCheckBox() {
+
+        if (UncheckedCheckBox.isSelected() == false) {
+            return true;
+        } else if (CheckedCheckBox.isSelected() == true) {
+            return true;
+        }
+        return false;
+    }
 
 }
