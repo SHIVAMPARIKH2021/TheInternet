6810377ae03cee5f52ce245396536a27cea99084
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-08-27 04:34:01 -0400

    used jackson api to test GET request's response

---
+++ src/main/java/testData/GetRequestTestData.java
@@ -0,0 +1,15 @@
+package testData;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.net.URL;
+
+public class GetRequestTestData {
+    public int page;
+    public int per_page;
+    public int total;
+    public int total_pages;
+    public Object data;
+    public Object support;
+}
