f68cdde011ff8c951e72ee3d24e0c35d67b2ae14
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-12-17 16:35:55 -0500

    restructuring dataBaseConnectivity package with abstraction

--- src/main/java/dataBaseConnectivity/Dao.java
+++ src/main/java/dataBaseConnectivity/Dao.java
@@ -7,46 +7,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
-public class Dao {
-    private static Connection connection;
-    private static Properties properties = new Properties();
-    private static Logger log = Logger.getLogger(Dao.class);
+public class Dao extends DbQuery {
 
-    /**
-     * Method to establish connection with database
-     * @return
-     */
-    private static Connection connect(){
-        String jdbcUrl = getUrl("url");
-        try{
-            connection = DriverManager.getConnection(jdbcUrl);
-        } catch (Exception e){
-            log.error("Database connectivity has been failed");
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    /**
-     * Method to get the url
-     * @param urlKey
-     * @return jdbc url
-     */
-    private static String getUrl(String urlKey){
-        String path = "\\src\\main\\java\\configurationpackage\\";
-        try {
-            properties.load(new FileInputStream(ResourcePathUtil.currentDirectory+path+"application.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String jdbcUrl =properties.getProperty(urlKey) +
-                ";databaseName="+properties.getProperty("databaseName") +
-                ";integratedSecurity="+properties.getProperty("integratedSecurity") +
-                ";encrypt="+properties.getProperty("encrypt") +
-                ";trustServerCertificate="+properties.getProperty("trustServerCertificate") +
-                ";username="+properties.getProperty("username") +
-                ";password="+properties.getProperty("password");
-        return jdbcUrl;
-    }
 }
