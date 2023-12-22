1d1346a12d31234d67ff01349f4559ddfa030ccb
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-12-22 04:34:50 -0500

    updating dao class and adding metod for xml file

--- src/main/java/dataBaseConnectivity/Dao.java
+++ src/main/java/dataBaseConnectivity/Dao.java
@@ -3,8 +3,33 @@ package dataBaseConnectivity;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.Properties;
 
 public class Dao extends DbQuery {
+    private static final String MS_SQL_FILE = "msSql.xml";
+    private static Dao dao = new Dao();
+    public Dao(){
+        super();
+    }
 
+    /**
+     * Method to get data from database.
+     * @param beanId of a sql query
+     * @param args of a sql query
+     * @return ResultSet
+     */
+    public static ResultSet getSql(String beanId, Object...args) {
+        return dao.readDatabase(MS_SQL_FILE,beanId,args);
+    }
+
+    /**
+     * Method to update database.
+     * @param beanId of a sql query
+     * @param args of a sql query
+     * @return number of updated rows
+     */
+    public static int sql(String beanId, Object...args) {
+        return dao.updateDatabase(MS_SQL_FILE,beanId,args);
+    }
 }
--- src/main/java/utility/CommonUtil.java
+++ src/main/java/utility/CommonUtil.java
@@ -4,7 +4,12 @@ import com.google.gson.Gson;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
-
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,7 +28,8 @@ public class CommonUtil {
     public static void getPathThroughProperties(String whichPropertyFile) {
         try {
             prop = new Properties();
-            file = new FileInputStream(currentDirectory + "\\src\\main\\java\\configurationpackage\\" + whichPropertyFile);
+            file = new FileInputStream(currentDirectory
+                    + "\\src\\main\\java\\configurationpackage\\" + whichPropertyFile);
             prop.load(file);
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,5 +77,81 @@ public class CommonUtil {
         Scanner scanner = new Scanner(in, StandardCharsets.UTF_8);
         scanner.useDelimiter("\\A");
         return scanner.next();
+    }
+
+    /**
+     * Method to read the xml file.
+     * Get the document builder.
+     * Get the document.
+     * @param fileName xml file name
+     * @return document object
+     */
+    public static Document readXml(String fileName){
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        Document document = null;
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            document = documentBuilder.parse(getResources(fileName));
+            if (document != null) {
+                document.getDocumentElement().normalize();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    /**
+     * Method to get node list by tag name of xml file.
+     * @param file file name
+     * @param tag xml file tag
+     * @return NodeList
+     */
+    public static NodeList getNodelistByTagName(String file, String tag){
+        return readXml(file).getElementsByTagName(tag);
+    }
+
+    /**
+     * Method to check query is read only or not.
+     * @param file file name
+     * @return true if query is read only
+     */
+    public static boolean isReadOnlyQuery(String file,String beanId) {
+        String isResultSet = "";
+        NamedNodeMap nodeMap;
+        NodeList list = getNodelistByTagName(file,"bean");
+        int tagLength = list.getLength();
+        for (int item = 0; item < tagLength; item++){
+            nodeMap = list.item(item).getAttributes();
+            boolean flag = list.item(item).getAttributes()
+                    .item(0).getNodeValue().equals(beanId);
+            if(flag) {
+                isResultSet = nodeMap.item(1).getNodeValue();
+                break;
+            }
+        }
+        return Boolean.parseBoolean(isResultSet);
+    }
+
+    /**
+     * Parse the xml structure.
+     * Get the elements of xml file by tag name.
+     * @param fileName file name of xml file
+     */
+    public static String getXmlValue(String fileName, String beanId){
+        NodeList nodeList = getNodelistByTagName(fileName,"bean");
+        Node node = null;
+        String query = "";
+        int nodeListLength = nodeList.getLength();
+        for (int item = 0;item < nodeListLength;item++) {
+            NamedNodeMap nodeMap = nodeList.item(item).getAttributes();
+            String nodeValue = nodeMap.item(0).getNodeValue();
+            if (nodeValue.equals(beanId)) {
+                query = getNodelistByTagName(fileName,"query")
+                        .item(item).getAttributes()
+                        .item(0).getNodeValue();
+            }
+        }
+        return query.trim();
     }
 }
